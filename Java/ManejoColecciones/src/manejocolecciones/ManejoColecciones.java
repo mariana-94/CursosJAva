5e4f9efe5303eb3dd404274ce35a9846a4023403
mariana-94 <rivera.ambrosio.mariana1994@gmail.com>
2020-08-27 13:33:02 -0500

    api colecciones

---
+++ Java/ManejoColecciones/src/manejocolecciones/ManejoColecciones.java
@@ -0,0 +1,19 @@
+
+package manejocolecciones;
+
+import java.util.*;
+
+public class ManejoColecciones {
+    
+    public static void main(String[] args) {
+        List milista = new ArrayList();
+        
+        milista.add(1);
+        milista.add(2);
+        milista.add(3);
+         //elemento repetido
+         milista.add(3);
+         
+         
+    }
+}
