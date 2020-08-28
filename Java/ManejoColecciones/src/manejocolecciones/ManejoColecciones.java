b3a742d22bfe297e1445b2a6d35a54f19cfa3d92
mariana-94 <rivera.ambrosio.mariana1994@gmail.com>
2020-08-28 15:52:23 -0500

    listas

--- Java/ManejoColecciones/src/manejocolecciones/ManejoColecciones.java
+++ Java/ManejoColecciones/src/manejocolecciones/ManejoColecciones.java
@@ -8,12 +8,25 @@ public class ManejoColecciones {
     public static void main(String[] args) {
         List milista = new ArrayList();
         
-        milista.add(1);
+        milista.add("1");
         milista.add(2);
         milista.add(3);
          //elemento repetido
          milista.add(3);
          
+         imprimir(milista);
          
+         Set miSet = new HashSet();
+         
+         
+         
+         
+    }
+    
+    private static void imprimir (Collection coleccion){
+        for(Object elemento: coleccion){
+            System.out.println("elemnto: " + elemento);
+        }
+        System.out.println("");
     }
 }
