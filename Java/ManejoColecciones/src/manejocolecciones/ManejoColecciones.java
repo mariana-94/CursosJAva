4402d4c0089506d5a02adfe045a4d7c65c2302c5
mariana-94 <rivera.ambrosio.mariana1994@gmail.com>
2020-09-01 13:31:07 -0500

    mapas

--- Java/ManejoColecciones/src/manejocolecciones/ManejoColecciones.java
+++ Java/ManejoColecciones/src/manejocolecciones/ManejoColecciones.java
@@ -4,7 +4,7 @@ package manejocolecciones;
 import java.util.*;
 
 public class ManejoColecciones {
-    
+    //set y listas mantienen el orden y no se necesita decir el numero de datos que debe contener
     public static void main(String[] args) {
         List milista = new ArrayList();
         
@@ -20,11 +20,24 @@ public class ManejoColecciones {
          miSet.add("100");
          miSet.add("200");
          miSet.add("300");
-         //elemento duplicado no se agrega
+         //elemento duplicado no se agrega 
          miSet.add("300");
-         imprimir(miSet);
-         
-         
+//         imprimir(miSet);
+
+        Map miMapa = new HashMap();
+        ///a diferencia de una lista maneja el concepto de llave valor
+        miMapa.put("valor1", "Juan");
+        miMapa.put("valor2", "David");
+        miMapa.put("valor3", "Niss");
+        //elemento duplicado sustituye al valor agregado previamente
+        miMapa.put("valor3", "Po");
+        //imprimimos las llaves
+        imprimir(miMapa.keySet());
+        //imprimimos los valores
+        imprimir(miMapa.values());
+        //imprimir un  valor asociado a una llave
+        System.out.println(miMapa.get("valor3"));  
+      
     }
     
     private static void imprimir (Collection coleccion){
